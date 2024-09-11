00d533c986c0c6e24d29d72b96d586d3456172b7
khalid karam <khalid.tasks@gmail.com>
2024-09-11 18:10:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NamedAndOptionalParamaters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayWelcome(repeat: 5, message: "welcome");
+        }
+
+        static void SayWelcome(string message = "hi", int repeat = 5)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+    }
+}
